134641d4cf39462892651a145f3fd2417b12f70e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-06-14 23:01:55 +0900

    n step with n complement, not 1 step.

--- p0.cc
+++ p0.cc
@@ -17,14 +17,10 @@ typedef SimpleFloat<uint64_t, DUInt<uint64_t, 64>, 64, Signed<DUInt<DUInt<DUInt<
 #include "simplelin.hh"
 #include "p0.hh"
 
+const auto range(20);
 int main(int argc, const char* argv[]) {
   std::string s;
-  int range(4);
-  if(1 < argc)
-    range = std::atoi(argv[1]);
-  P0B<num_t> p(range * 3);
-  P0B<num_t> q(range * 4);
-  P0B<num_t> r(range * 5);
+  P0B<num_t, 6> p(range);
   num_t d0(0);
   auto  bd(d0);
   auto  MM(d0);
@@ -34,7 +30,7 @@ int main(int argc, const char* argv[]) {
     ins >> d;
     if(d != bd) {
       d0 += (d - bd) * MM;
-      MM  = (p.next(d) + q.next(d) + r.next(d)) / num_t(3) - d;
+      MM  = p.next(d) - d;
     }
     std::cout << d0 << "," << MM << std::endl;
     bd = d;
