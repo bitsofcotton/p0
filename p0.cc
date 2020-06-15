5b88ddd6f5491fd4ccba92f5e0a8633b52913581
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-06-16 00:48:45 +0900

    complement with circular revert.

--- p0.cc
+++ p0.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <iomanip>
 #include "assert.h"
 
 #include <complex>
@@ -12,27 +13,39 @@ typedef long double num_t;
 /*
 #include "ifloat.hh"
 template <typename T> using complex = Complex<T>;
-typedef SimpleFloat<uint64_t, DUInt<uint64_t, 64>, 64, Signed<DUInt<DUInt<DUInt<DUInt<uint64_t, 64>, 128>, 256>, 512>, 1024> > num_t;
+typedef SimpleFloat<uint64_t, DUInt<uint64_t, 64>, 64, int32_t> num_t;
 */
 #include "simplelin.hh"
 #include "p0.hh"
 
-const auto range(20);
 int main(int argc, const char* argv[]) {
   std::string s;
-  P0B<num_t, 6> p(range);
+  int range(40);
+  int loop(8);
+  if(1 < argc)
+    range = std::atoi(argv[1]);
+  if(2 < argc)
+    loop  = std::atoi(argv[2]);
+  P0C<num_t, P0B<num_t> > p(range, loop);
   num_t d0(0);
-  auto  bd(d0);
   auto  MM(d0);
+  auto  Md(d0);
+  auto  bd(d0);
+  int   t(0);
   while(std::getline(std::cin, s, '\n')) {
     num_t d;
     std::stringstream ins(s);
     ins >> d;
-    if(d != bd) {
-      d0 += (d - bd) * MM;
-      MM  = p.next(d) - d;
+    if(bd != 0)
+      Md = max(Md, abs(d - bd) * num_t(4));
+    if(d != bd && Md != 0) {
+      if(! isnan(MM) && MM != 0)
+        d0 += (d - bd) * MM;
+      MM  = p.next(d / Md) * Md - d;
+      if(! isfinite(MM) || isnan(MM) || t ++ < range * 2)
+        MM = num_t(0) / num_t(0);
     }
-    std::cout << d0 << "," << MM << std::endl;
+    std::cout << d0 << ", " << MM << ", " << Md << std::endl;
     bd = d;
   }
   return 0;
