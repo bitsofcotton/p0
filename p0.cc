5f37e7fd19ca391944e8711cac290083e044b143
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-04-16 23:12:20 +0900

    merge externals, no logic change.

--- p0.cc
+++ p0.cc
@@ -3,57 +3,12 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <map>
 #include <iomanip>
 #include <assert.h>
 
-#if !defined(_FLOAT_BITS_)
-  #include <complex>
-  #include <cmath>
-  using namespace std;
-  typedef long double num_t;
-#else
-  #include "ifloat.hh"
-  template <typename T> using complex = Complex<T>;
-# if _FLOAT_BITS_ == 8
-    typedef uint8_t myuint;
-    typedef int8_t  myint;
-    typedef SimpleFloat<myuint, uint16_t, 8, int64_t> num_t;
-    #define mybits 8
-# elif _FLOAT_BITS_ == 16
-    typedef uint16_t myuint;
-    typedef int16_t  myint;
-    typedef SimpleFloat<myuint, uint32_t, 16, int64_t> num_t;
-    #define mybits 16
-# elif _FLOAT_BITS_ == 32
-    typedef uint32_t myuint;
-    typedef int32_t  myint;
-    typedef SimpleFloat<myuint, uint64_t, 32, int64_t> num_t;
-    #define mybits 32
-# elif _FLOAT_BITS_ == 64
-    typedef uint64_t myuint;
-    typedef int64_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 64>, 64, int64_t> num_t;
-    #define mybits 64
-# elif _FLOAT_BITS_ == 128
-    typedef DUInt<uint64_t, 64> uint128_t;
-    typedef Signed<uint128_t, 128> int128_t;
-    typedef uint128_t myuint;
-    typedef int128_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 128>, 128, int64_t> num_t;
-    #define mybits 128
-# elif _FLOAT_BITS_ == 256
-    typedef DUInt<uint64_t, 64> uint128_t;
-    typedef DUInt<uint128_t, 128> uint256_t;
-    typedef Signed<uint256_t, 128> int256_t;
-    typedef uint256_t myuint;
-    typedef int256_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 256>, 256, int64_t> num_t;
-    #define mybits 256
-# else
-#   error cannot handle float
-# endif
-#endif
-
+#include "ifloat.hh"
+typedef myfloat num_t;
 #include "simplelin.hh"
 #include "p0.hh"
 
