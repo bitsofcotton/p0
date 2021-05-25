40ffbb05869ba87a4941d124708bf4c7c6ad328a
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-05-25 23:09:59 +0900

    code clean, INPUT CHANGE.

--- p0.cc
+++ p0.cc
@@ -36,24 +36,23 @@ int main(int argc, const char* argv[]) {
   auto  M(d);
   int   t(0);
   while(std::getline(std::cin, s, '\n')) {
-    const auto bd(d);
     std::stringstream ins(s);
     ins >> d;
-    if(d != bd) {
-      if(bd != num_t(0) && M != num_t(0)) {
-        s0 += (d - bd) - M;
-        s1 += (s2 = (d - bd) * M);
+    if(d != num_t(0)) {
+      if(M != num_t(0)) {
+        s0 += d - M;
+        s1 += (s2 = d * M);
       }
       const auto pn((var < 0 ? (whole ? qw0.next(d) : qp0.next(d))
-                             : (whole ? pw0.next(d) : pp0.next(d))) - d);
+                             : (whole ? pw0.next(d) : pp0.next(d))));
       // original function lower and higher frequency part, middle is ignored.
       if(d == num_t(0)) M = pn;
       else if(whole)
          M = (pn + num_t(1) / (var < 0 ? qw1.next(num_t(1) / d)
-                                      : pw1.next(num_t(1) / d)) - d) / num_t(2);
+                                      : pw1.next(num_t(1) / d))) / num_t(2);
       else
          M = (pn + num_t(1) / (var < 0 ? qp1.next(num_t(1) / d)
-                                      : pp1.next(num_t(1) / d)) - d) / num_t(2);
+                                      : pp1.next(num_t(1) / d))) / num_t(2);
       if(! isfinite(M) || isnan(M)) M = pn;
       if(t ++ < abs(var)) M = num_t(0);
     }
