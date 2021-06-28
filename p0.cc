d54f02c7b5a61a82563b019c55bc955be08a5d3e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-06-28 14:03:52 +0900

    code clean, reclose with this.

--- p0.cc
+++ p0.cc
@@ -36,44 +36,35 @@ int main(int argc, const char* argv[]) {
   auto  s2(d);
   auto  s3(d);
   auto  M(d);
-  auto  MM(d);
-  auto  bd(d);
   int   t(0);
   while(std::getline(std::cin, s, '\n')) {
+    const auto bd(d);
     std::stringstream ins(s);
     ins >> d;
-    const auto bdd(d);
-    const auto bMM(MM);
-    if(MM < abs(d))
-      MM = abs(d) * num_t(2);
-    d -= bd;
-    if(bMM != num_t(0))
-      d /= bMM;
-    if(d != num_t(0)) {
+    if(d != bd) {
       if(M != num_t(0)) {
-        s0 += (s3 = d - M);
-        s1 += (s2 = d * M);
+        s0 += (s3 = (d - bd) - M);
+        s1 += (s2 = (d - bd) * M);
       }
       const auto pn((var < 0 ? (whole ? qw0.next(d) : qp0.next(d))
-                             : (whole ? pw0.next(d) : pp0.next(d))));
+                             : (whole ? pw0.next(d) : pp0.next(d))) - d);
       // original function lower and higher frequency part, middle is ignored.
       if(d == num_t(0)) M = pn;
       else if(whole) {
          const auto denom(var < 0 ? qw1.next(num_t(1) / d)
                                   : pw1.next(num_t(1) / d));
          M = denom == num_t(0) ? pn :
-           (pn + num_t(1) / denom) / num_t(2);
+           (pn + num_t(1) / denom - d) / num_t(2);
       } else {
          const auto denom(var < 0 ? qp1.next(num_t(1) / d)
                                   : pp1.next(num_t(1) / d));
          M = denom == num_t(0) ? pn :
-           (pn + num_t(1) / denom) / num_t(2);
+           (pn + num_t(1) / denom - d) / num_t(2);
       }
       if(! isfinite(M) || isnan(M)) M = pn;
       if(t ++ < abs(var)) M = num_t(0);
     }
-    std::cout << (MM != num_t(0) ? M / MM : M) << "," << s0 << ", " << s1 << ", " << s2 << ", " << s3 << ", " << std::endl << std::flush;
-    bd = bdd;
+    std::cout << M << "," << s0 << ", " << s1 << ", " << s2 << ", " << s3 << ", " << std::endl << std::flush;
   }
   return 0;
 }
