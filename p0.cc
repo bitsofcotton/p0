9cf1059caa7512601391f4b6fd6a7b87ea02fa50
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-08-28 19:20:48 +0900

    pow ratio revert.

--- p0.cc
+++ p0.cc
@@ -17,10 +17,12 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
   int   var(3);
+  int   pw(1);
   if(argc < 2)
-    std::cerr << "p0 <var>?" << std::endl;
+    std::cerr << "p0 <var>? <pow>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
-  std::cerr << "continue with p0 " << var << std::endl;
+  if(2 < argc) pw  = std::atoi(argv[2]);
+  std::cerr << "continue with p0 " << var << " " << pw << std::endl;
   P0<num_t, linearFeeder<num_t, sumFeeder<num_t> > > p(abs(var));
   P0<num_t, arctanFeeder<num_t, sumFeeder<num_t> > > q(abs(var));
   num_t d(0);
@@ -33,6 +35,7 @@ int main(int argc, const char* argv[]) {
     // to make any sub-sequences to be the same meaning, no inverse condition,
     // this causes middle and high frequency parts to be ignored.
     M = var < 0 ? q.next(d) : p.next(d);
+    M = pw == 0 ? sgn<num_t>(M) : sgn<num_t>(M) * pow(abs(M), pw < 0 ? num_t(1) / num_t(abs(pw)) :  num_t(pw));
     std::cout << D << ", " << M << ", " << d << std::endl << std::flush;
   }
   return 0;
