2676317f4a3be028267d15e9e5ccada9b67c657e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-21 21:31:18 +0900

    integrate iface.py .

--- p0.cc
+++ p0.cc
@@ -16,21 +16,21 @@ typedef myfloat num_t;
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
-  const auto var(25);
+  const auto var(10);
         int  step(1);
   if(argc < 2)
     std::cerr << "p0 <step>?" << std::endl;
   if(1 < argc) step = std::atoi(argv[1]);
   std::cerr << "continue with p0 " << step << std::endl;
-  P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > p(abs(var), abs(step));
-  P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > q(abs(var), abs(step));
+  shrinkMatrix<num_t, P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > p(P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > >(abs(var), abs(step)), abs(step));
+  shrinkMatrix<num_t, P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > q(P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > >(abs(var), abs(step)), abs(step));
   num_t d(0);
+  auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
-    // to make any sub-sequences to be the same meaning, no inverse condition,
-    // this causes middle and high frequency parts to be ignored.
-    std::cout << (step < 0 ? q.next(d) : p.next(d)) << ", " << d << std::endl << std::flush;
+    const auto D(d * M);
+    std::cout << D << ", " << (M = step < 0 ? q.next(d) : p.next(d)) << ", " << d << std::endl << std::flush;
   }
   return 0;
 }
