d31dd7a8de4aba06675cbddf1d01f6cc002b5e82
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-22 23:13:16 +0900

    revert to version without shrinkMatrix.

--- p0.cc
+++ p0.cc
@@ -16,21 +16,20 @@ typedef myfloat num_t;
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
-  const auto var(10);
-        int  step(1);
+  auto var(10);
   if(argc < 2)
-    std::cerr << "p0 <step>?" << std::endl;
-  if(1 < argc) step = std::atoi(argv[1]);
-  std::cerr << "continue with p0 " << step << std::endl;
-  shrinkMatrix<num_t, P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > p(P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > >(abs(var), abs(step)), abs(step));
-  shrinkMatrix<num_t, P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > q(P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > >(abs(var), abs(step)), abs(step));
+    std::cerr << "p0 <len>?" << std::endl;
+  if(1 < argc) var = std::atoi(argv[1]);
+  std::cerr << "continue with p0 " << var << std::endl;
+  P0<num_t, linearFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > p(abs(var));
+  P0<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > q(abs(var));
   num_t d(0);
   auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    std::cout << D << ", " << (M = step < 0 ? q.next(d) : p.next(d)) << ", " << d << std::endl << std::flush;
+    std::cout << D << ", " << (M = var < 0 ? q.next(d) : p.next(d)) << ", " << d << std::endl << std::flush;
   }
   return 0;
 }
