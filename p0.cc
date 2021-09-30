1e0c8caf0ed2da0990bbc8bd0250b700b9842877
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-30 18:42:53 +0900

    optimal varlen.

--- p0.cc
+++ p0.cc
@@ -16,20 +16,21 @@ typedef myfloat num_t;
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
-  auto var(10);
+  int var(7);
   if(argc < 2)
     std::cerr << "p0 <len>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
   std::cerr << "continue with p0 " << var << std::endl;
-  P0<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(abs(var));
-  P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > q(abs(var));
+  assert(0 < var);
+  P0<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(var);
+  P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > q(var);
   num_t d(0);
   auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    std::cout << D << ", " << (M = var < 0 ? q.next(d) : p.next(d)) << ", " << d << std::endl << std::flush;
+    std::cout << D << ", " << (M = p.next(d) + q.next(d)) << ", " << d << std::endl << std::flush;
   }
   return 0;
 }
