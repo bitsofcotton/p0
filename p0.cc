ab4c7e58d9b3d8c6a5b18de849ae234029d69df5
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-01-22 23:22:20 +0900

    no use p0d, rewrite to hack cont. cond.
    to hack better, invert original continuous one.

--- p0.cc
+++ p0.cc
@@ -21,15 +21,17 @@ int main(int argc, const char* argv[]) {
     std::cerr << argv[0] << " <len>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
   std::cerr << "continue with " << argv[0] << " " << var << std::endl;
-  P0<num_t, idFeeder<num_t> > p(abs(var));
-  P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > q(abs(var));
+  P0D<num_t, P0<num_t, idFeeder<num_t> > > p(abs(var));
+  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q(abs(var));
   num_t d(0);
   auto  M(d);
+  auto  S(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    std::cout << D << ", " << (M = var < 0 ? q.next(d) : p.next(d)) << ", " << s.substr((int)ins.tellg() + 1) << std::endl << std::flush;
+    const auto S0(S);
+    std::cout << D << ", " << (M = var < 0 ? q.next(S += d) - S0 : p.next(S += d) - S0) << ", " << s.substr((int)ins.tellg() + 1) << std::endl << std::flush;
   }
   return 0;
 }
--- p0d.cc
+++ p0d.cc
@@ -17,14 +17,12 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
   auto var(3);
-  auto rnd(1);
   if(argc < 3)
-    std::cerr << argv[0] << " <len>? <rnd?>" << std::endl;
+    std::cerr << argv[0] << " <len>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
-  if(2 < argc) rnd = std::atoi(argv[2]);
-  std::cerr << "continue with " << argv[0] << " " << var << " " << rnd << std::endl;
-  P0D<num_t, P0<num_t, idFeeder<num_t> > > p(abs(var), 1, rnd);
-  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q(abs(var), 1, rnd);
+  std::cerr << "continue with " << argv[0] << " " << var << std::endl;
+  P0D<num_t, P0<num_t, idFeeder<num_t> > > p(abs(var));
+  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q(abs(var));
   num_t d(0);
   auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
