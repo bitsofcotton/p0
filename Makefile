49379f30a4a5e473c9f0f9bd67ab2e51dec60241
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-01-26 22:32:48 +0900

    p02 instead of step.

--- Makefile
+++ Makefile
@@ -11,12 +11,16 @@ LDFLAGS+=	-lc++
 #CXXFLAGS+=	-D_FLOAT_BITS_=32
 
 clean:
-	@rm -rf p0 p0-32 p0-r p0-r-32
-all:	p0 p0-32 p0-r p0-r-32
+	@rm -rf p0 p0-32 p02 p02-32 p0-r p0-r-32
+all:	p0 p0-32 p02 p02-32 p0-r p0-r-32
 p0:
 	${CXX} ${CXXFLAGS} -static -o p0 p0.cc
 p0-32:
 	${CXX} ${CXXFLAGS} -static -D_FLOAT_BITS_=32 -o p0-32 p0.cc
+p02:
+	${CXX} ${CXXFLAGS} -static -o p02 p02.cc
+p02-32:
+	${CXX} ${CXXFLAGS} -static -D_FLOAT_BITS_=32 -o p02-32 p02.cc
 p0-r:
 	${CXX} ${CXXFLAGS} -static -o p0-r p0-r.cc
 p0-r-32:
--- p02.cc
+++ p02.cc
@@ -21,17 +21,23 @@ int main(int argc, const char* argv[]) {
     std::cerr << argv[0] << " <len>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
   std::cerr << "continue with " << argv[0] << " " << var << std::endl;
-  P0D<num_t, P0<num_t, idFeeder<num_t> > > p(abs(var));
-  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q(abs(var));
+  P0D<num_t, P0<num_t, idFeeder<num_t> > > p1(abs(var));
+  P0D<num_t, P0<num_t, idFeeder<num_t> > > p2(abs(var), 2);
+  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q1(abs(var));
+  P0D<num_t, P0<num_t, deltaFeeder<num_t, arctanFeeder<num_t, sumFeeder<num_t, idFeeder<num_t> > > > > > q2(abs(var), 2);
   num_t d(int(0));
   auto  M(d);
+  auto  bM(d);
   auto  S(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
-    const auto D(d * M);
-    const auto S0(S);
-    std::cout << D << ", " << (M = var < 0 ? q.next(S += d) - S0 : p.next(S += d) - S0) << ", " << s.substr((int)ins.tellg() + 1) << std::endl << std::flush;
+    const auto D(d * bM);
+    S += d; bM = M;
+    std::cout << D << ", " <<
+      (M = var < 0 ? q2.next(S) - q1.next(S)
+                   : p2.next(S) - p1.next(S)) << ", " <<
+      s.substr((int)ins.tellg() + 1) << std::endl << std::flush;
   }
   return 0;
 }
