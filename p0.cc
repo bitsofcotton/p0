0b6136c247a933a4c010e58f709b212b18c2a40e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-10 18:26:30 +0900

    northpole on prob and value each once.

--- p0.cc
+++ p0.cc
@@ -12,7 +12,6 @@
 #include "lieonn.hh"
 typedef myfloat num_t;
 #include "p0.hh"
-typedef shrinkMatrix<num_t, P0<num_t, idFeeder<num_t> > > p0_t;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
@@ -22,31 +21,31 @@ int main(int argc, const char* argv[]) {
     std::cerr << argv[0] << " <len>?" << std::endl;
   if(1 < argc) var  = std::atoi(argv[1]);
   std::cerr << "continue with " << argv[0] << " " << var << std::endl;
-  northPole<num_t, northPole<num_t, p0_t> > p(northPole<num_t, p0_t>(p0_t(P0<num_t, idFeeder<num_t> >(var < 0 ? - var : 3), var < 0 ? 1 : var)));
+  northPole<num_t, shrinkMatrix<num_t, northPole<num_t, P0<num_t, idFeeder<num_t> > > > > p(shrinkMatrix<num_t, northPole<num_t, P0<num_t, idFeeder<num_t> > > >(northPole<num_t, P0<num_t, idFeeder<num_t> > >(P0<num_t, idFeeder<num_t> >(var < 0 ? - var : 3)), var < 0 ? 1 : var));
   int   t;
   num_t d(t ^= t);
   auto  M(d);
   auto  S(d);
-  auto  Snote(d);
+  auto  A(d);
   vector<num_t> h;
   h.resize(abs(var) * 2 + 3, num_t(int(0)));
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    Snote += d;
-    if(d != num_t(int(0))) {
-      h[(t ++) % h.size()] = d;
-      if(! (t % h.size())) {
-        S = num_t(int(0));
-        for(int i = 0; i < h.size() - 1; i ++)
-          (void)p.next(S += h[i] - Snote / num_t(int(t)));
-      }
-      const auto S0(S);
-      auto pn(p.next(S += d - Snote / num_t(int(t))));
-      std::cout << D << ", " << (pn == num_t(int(0)) ? M : M = pn - S0 + Snote / num_t(int(t))) << std::endl << std::flush;
-    } else
-      std::cout << D << ", " << M << std::endl << std::flush;
+    h[(t ++) % h.size()] = d;
+    if(h.size() <= t) {
+      A = S = num_t(int(0));
+      for(int i = 0; i < h.size(); i ++)
+        A += h[i];
+      A /= num_t(int(h.size()));
+      for(int i = 0; i < h.size() - 1; i ++)
+        (void)p.next(S += h[i] - A);
+      t ^= t;
+    }
+    const auto S0(S);
+    auto pn(p.next(S += d - A));
+    std::cout << D << ", " << (pn == num_t(int(0)) ? M : M = pn - S0 + A) << std::endl << std::flush;
   }
   return 0;
 }
