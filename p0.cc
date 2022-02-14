dd9647590e2fa993ce0d273604ebe335d36b2df5
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-14 22:47:43 +0900

    class capsule, northpole to delta, sum.

--- p0.cc
+++ p0.cc
@@ -15,32 +15,28 @@ typedef myfloat num_t;
 typedef P0<num_t, idFeeder<num_t> > p0_0t;
 typedef shrinkMatrix<num_t, p0_0t, true> p0_1t;
 typedef northPole<num_t, p0_1t> p0_2t;
-typedef northPole<num_t, p0_2t> p0_t;
+typedef sumChain<num_t, p0_2t> p0_3t;
+typedef northPole<num_t, p0_3t> p0_4t;
+typedef avgOrigin<num_t, myuint, p0_4t> p0_t;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
   auto var(1);
-  auto le(1);
+  auto le(0);
   if(argc < 2)
     std::cerr << argv[0] << " <len>? <Marctan>?" << std::endl;
   if(1 < argc) var = std::atoi(argv[1]);
   if(2 < argc) le  = std::atoi(argv[2]);
   std::cerr << "continue with " << argv[0] << " " << var << " " << le << std::endl;
-  assert(0 <= le);
-  p0_t  p(p0_2t(p0_1t(p0_0t(var < 0 ? - var : 3), var < 0 ? 1 : var)), pow(num_t(int(2)), num_t(int(le))));
-  int   t;
-  num_t d(t ^= t);
+  p0_t  p(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(var < 0 ? - var : 3), var < 0 ? 1 : var), pow(num_t(int(2)), num_t(int(le))))), pow(num_t(int(2)), num_t(int(le)))));
+  num_t d(int(0));
   auto  M(d);
-  auto  A(d);
-  auto  S(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    const auto S0(S);
-    A += d; t ++;
-    std::cout << D << ", " << (M = p.next(S += d - A / num_t(t)) - S0 + A / num_t(t)) << std::endl << std::flush;
+    std::cout << D << ", " << (M = p.next(d)) << std::endl << std::flush;
   }
   return 0;
 }
