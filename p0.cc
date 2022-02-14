4d3508b7ffdb36513f28dc1f0efcc69898ef24fb
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-14 18:02:59 +0900

    we need huge accuracy with recursive long range.

--- p0.cc
+++ p0.cc
@@ -14,7 +14,8 @@ typedef myfloat num_t;
 #include "p0.hh"
 typedef P0<num_t, idFeeder<num_t> > p0_0t;
 typedef shrinkMatrix<num_t, p0_0t, true> p0_1t;
-typedef northPole<num_t, p0_1t> p0_t;
+typedef northPole<num_t, p0_1t> p0_2t;
+typedef northPole<num_t, p0_2t> p0_t;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
@@ -27,7 +28,7 @@ int main(int argc, const char* argv[]) {
   if(2 < argc) le  = std::atoi(argv[2]);
   std::cerr << "continue with " << argv[0] << " " << var << " " << le << std::endl;
   assert(0 <= le);
-  p0_t  p(p0_1t(p0_0t(var < 0 ? - var : 3), var < 0 ? 1 : var), pow(num_t(int(2)), num_t(int(le))));
+  p0_t  p(p0_2t(p0_1t(p0_0t(var < 0 ? - var : 3), var < 0 ? 1 : var), pow(num_t(int(2)), num_t(int(le)))), pow(num_t(int(2)), num_t(int(le))));
   int   t;
   num_t d(t ^= t);
   auto  M(d);
