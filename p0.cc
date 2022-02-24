01416064cad55f4f720c89f5f583a8f80fc4a98d
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-24 18:01:03 +0900

    merge latest lieonn, no binary change.

--- p0.cc
+++ p0.cc
@@ -22,31 +22,27 @@ typedef P0<num_t, idFeeder<num_t> > p0_0t;
 typedef shrinkMatrix<num_t, p0_0t, true> p0_1t;
 typedef northPole<num_t, p0_1t> p0_2t;
 typedef sumChain< num_t, p0_2t> p0_3t;
-typedef northPole<num_t, p0_3t> p0_t;
+typedef northPole<num_t, p0_3t> p0_4t;
+typedef sumChain< num_t, p0_4t, true> p0_t;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
-  p0_t   p(p0_3t(p0_2t(p0_1t(p0_0t(3)) )));
-  myuint t;
-  num_t  d(t ^= t);
+  p0_t   p(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(3)) ))) );
+  num_t  d(int(0));
   auto   M(d);
   auto   Mx(d);
-  auto   S(d);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
     if(Mx < abs(d)) Mx = abs(d) * num_t(int(2));
-    const auto A((S += d) / num_t(++ t));
-    // N.B. predict any R to R on 1~3 markov.
-          auto pn(p.next(d - A) + A);
-    if(isfinite(pn) &&
-       isfinite(pn) && - Mx / sqrt(sqrt(SimpleMatrix<num_t>().epsilon)) &&
-                    pn < Mx / sqrt(sqrt(SimpleMatrix<num_t>().epsilon)) )
+          auto pn(p.next(d));
+    if(isfinite(pn) && - Mx / sqrt(sqrt(SimpleMatrix<num_t>().epsilon)) < pn
+                 && pn < Mx / sqrt(sqrt(SimpleMatrix<num_t>().epsilon)) )
       M = max(- Mx, min(Mx, move(pn)));
     else M = num_t(int(0));
-    std::cout << D << ", " << M << std::endl << std::flush;
+    std::cout << D << ", " << (M /= Mx != num_t(int(0)) ? Mx : num_t(int(1))) << std::endl << std::flush;
   }
   return 0;
 }
