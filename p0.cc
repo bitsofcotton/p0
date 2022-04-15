1c7dcc0b74ab7100f1c62d660fcf853f27b16386
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-04-15 22:32:00 +0900

    numerical ok with this, elim prob wrap on former.

--- p0.cc
+++ p0.cc
@@ -20,7 +20,7 @@ typedef myfloat num_t;
 //      even if the parameter on P0 is large, situation unchange.
 //      so we should use sectional measurement for them.
 typedef P0<num_t, idFeeder<num_t> > p0_0t;
-// N.B. sectional measurement.
+// N.B. sectional measurement, also expected value.
 typedef shrinkMatrix<num_t, p0_0t> p0_1t;
 // N.B. make information-rich not to associative/commutative.
 //      2 dimension semi-order causes (x, status) from input as sedenion.
@@ -36,10 +36,8 @@ typedef sumChain<num_t, p0_7t>  p0_8t;
 // N.B. we treat periodical part as non aligned complex arg part.
 typedef logChain<num_t, p0_8t>  p0_9t;
 typedef logChain<num_t, p0_9t>  p0_10t;
-// N.B. we take probability on whole:
-typedef shrinkMatrix<num_t, p0_10t> p0_11t;
 // N.B. we take average as origin of input.
-typedef sumChain<num_t, p0_11t, true> p0_t;
+typedef sumChain<num_t, p0_10t, true> p0_t;
 
 // N.B. plain complex form.
 typedef northPole<num_t, p0_1t>  p0_s2t;
@@ -47,24 +45,23 @@ typedef northPole<num_t, p0_s2t> p0_s3t;
 typedef sumChain<num_t, p0_s3t>  p0_s4t;
 typedef logChain<num_t, p0_s4t>  p0_s5t;
 typedef logChain<num_t, p0_s5t>  p0_s6t;
-typedef shrinkMatrix<num_t, p0_s6t> p0_s7t;
-typedef sumChain<num_t, p0_s7t, true> p0_st;
+typedef sumChain<num_t, p0_s6t, true> p0_st;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
-  int var(1);
+  int var(2);
   if(argc <= 1) std::cerr << argv[0] << " <mid>? : continue with ";
-  if(1 < argc) var  = std::atoi(argv[1]);
+  if(1 < argc) var = std::atoi(argv[1]);
   std::cerr << argv[0] << " " << var << std::endl;
   // N.B. this is not optimal but we use this:
-  const int step(max(num_t(3), exp(log(num_t(abs(var * 2))) * log(num_t(abs(var * 2))))));
+  const int step(max(num_t(3), exp(log(num_t(abs(var))) * log(num_t(abs(var))))));
   p0_t  p;
   p0_st q;
   if(var < 0)
-    q = p0_st(p0_s7t(p0_s6t(p0_s5t(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var * 2)), abs(var)) )) ) )), abs(var)));
+    q = p0_st(p0_s6t(p0_s5t(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var)), abs(var)) )) ) )) );
   else
-    p = p0_t(p0_11t(p0_10t(p0_9t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var * 2)), abs(var)), step), step), step), step) )) ) )), abs(var)) );
+    p = p0_t(p0_10t(p0_9t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var)), abs(var)), abs(var)), abs(var)), abs(var)), abs(var)) )) ) )) );
   num_t d(int(0));
   auto  Mx(d);
   auto  M(d);
