93966bab34ea80d0fd376d90c591909b92e3f6ad
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-04-15 17:40:12 +0900

    fix prediction step, it had pred. before steps.

--- p0.cc
+++ p0.cc
@@ -53,7 +53,7 @@ int main(int argc, const char* argv[]) {
   if(1 < argc) var   = std::atoi(argv[1]);
   if(2 < argc) look  = std::atoi(argv[2]);
   std::cerr << argv[0] << " " << var << " " << look << std::endl;
-  look += look + (look < 0 ? - 1 : 1);
+  look += look;
   assert(look);
   // N.B. this is not optimal but we use this:
   const int  step(max(num_t(3), exp(log(num_t(abs(var * 2) + abs(look) - 1)) * log(num_t(abs(var * 2) + abs(look) - 1)))));
@@ -65,14 +65,14 @@ int main(int argc, const char* argv[]) {
   p0_ast qq;
   if(look < 0) {
     if(var < 0)
-      qq = p0_ast(p0_st(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var) + abs(look) - 1), abs(var)) )) ), abs(var)));
+      qq = p0_ast(p0_st(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var * 2) + abs(look) - 1), abs(var)) )) ), abs(var)));
     else
-      pp = p0_st(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var) + abs(look) - 1), abs(var)) )) ), abs(var));
+      pp = p0_st(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(step, abs(var * 2) + abs(look) - 1), abs(var)) )) ), abs(var));
   } else {
     if(var < 0)
-      q = p0_at(p0_t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var) + abs(look) - 1), abs(var)), step), step), step), step) )) ), abs(var)) );
+      q = p0_at(p0_t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var * 2) + abs(look) - 1), abs(var)), step), step), step), step) )) ), abs(var)) );
     else
-      p = p0_t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var) + abs(look) - 1), abs(var)), step), step), step), step) )) ), abs(var));
+      p = p0_t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(step, abs(var * 2) + abs(look) - 1), abs(var)), step), step), step), step) )) ), abs(var));
   }
   num_t din(int(0));
   auto  Mx(din);
@@ -85,11 +85,10 @@ int main(int argc, const char* argv[]) {
     const auto bd(din);
     std::stringstream ins(s);
     ins >> din;
-    num_t D(int(0));
     for(int i = 0; i < 2; i ++) {
       const auto  d(i ? din + din : din + bd);
             num_t dd(int(0));
-      D += d * M[0];
+      const auto  D(d * M[0]);
       Mx = max(Mx, abs(d) * num_t(int(2)));
       if(recur) {
         d0[0] += d;
@@ -109,9 +108,9 @@ int main(int argc, const char* argv[]) {
       else M[M.size() - 1] = num_t(int(0));
       if(recur) for(int i = 0; i < d0.size(); i ++) M[M.size() - 1] *= d0[i];
       M[M.size() - 1] = max(- Mx, min(Mx, M[M.size() - 1]));
+      if(i) std::cout << D << ", " << M[M.size() - 1] << std::endl;
       d1 = d0;
     }
-    std::cout << D << ", " << M[M.size() - 1] << ", " << M[M.size() - 2] << std::endl << std::flush;
   }
   return 0;
 }
