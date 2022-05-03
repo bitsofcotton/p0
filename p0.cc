66522a887fa7c90a1112133c62a4868e7884c82e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-05-03 19:17:39 +0900

    fix summation and average brk and plain.

--- p0.cc
+++ p0.cc
@@ -59,12 +59,12 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   std::string s;
   int status(60);
-  int break_invariant0(0);
+  int break_invariant0(600);
   if(argc <= 1) std::cerr << argv[0] << " <status>? <brk>? : continue with ";
   if(1 < argc) status = std::atoi(argv[1]);
   if(2 < argc) break_invariant0 = std::atoi(argv[2]);
   std::cerr << argv[0] << " " << status << " " << break_invariant0 << std::endl;
-  assert(status);
+  assert(status && break_invariant0);
   if(status < 0)
     status = int(max(num_t(3), ceil(exp(log(num_t(- status)) * log(num_t(- status))))));
   int   t;
@@ -77,43 +77,45 @@ int main(int argc, const char* argv[]) {
   if(status <= 3) {
     std::cerr << "using plain prediction:" << std::endl;
     p0_0t p(status);
+    auto  pp(p);
     while(std::getline(std::cin, s, '\n')) {
       std::stringstream ins(s);
       ins >> d;
       const auto D(d * M);
-      const auto r(break_invariant0 ? one + num_t(int(t ++)) / num_t(int(abs(break_invariant0))) : one);
-      std::cout << D << ", " << (M = break_invariant0 ? (break_invariant0 < 0 ? p.next(d / r) * r : p.next(d * r) / r) : p.next(d)) << ", " << (S += D) << std::endl << std::flush;
+      const auto r(one + num_t(int(t ++)) / num_t(int(abs(break_invariant0))));
+      std::cout << D << ", " << (M = ((break_invariant0 < 0 ? p.next(d / r) * r : p.next(d * r) / r) + pp.next(d)) / num_t(int(2))) << ", " << (S += D) << std::endl << std::flush;
     }
     return 0;
   }
   // N.B. this is not optimal but we use this:
   const int var(max(num_t(1), exp(sqrt(log(num_t(status))))));
-  p0_t  p, pp;
-  p0_st q, qq;
-  auto  dS(d);
+  p0_t  p, pp, p2, pp2;
+  p0_st q, qq, q2, qq2;
+  auto  Sd(d);
+  auto  Sr(d);
+  auto  Sri(d);
   bool  need_init(true);
-  SimpleMatrix<num_t> Mstore(8, max(8, status));
+  SimpleMatrix<num_t> Mstore(16, max(16, status));
   Mstore.O();
   while(std::getline(std::cin, s, '\n')) {
     if(need_init) {
       p  = p0_t(p0_10t(p0_9t(p0_8t(p0_7t(p0_6t(p0_5t(p0_4t(p0_3t(p0_2t(p0_1t(p0_0t(status, var), var), var), var), var), var) )) ) )) );
       q  = p0_st(p0_s6t(p0_s5t(p0_s4t(p0_s3t(p0_s2t(p0_1t(p0_0t(status, var), var) )) ) )) );
-      pp = p;
-      qq = q;
+      pp2 = p2 = pp = p;
+      qq2 = q2 = qq = q;
       need_init = false;
       std::cerr << "using real status as: " << status + var * 4 << std::endl;
     }
     std::stringstream ins(s);
     ins >> d;
     const auto D(d * M);
-    const auto bdS(dS);
-    dS += d;
-    if(bdS == zero || dS == zero) {
+    if(d == zero) {
       std::cout << D << ", " << M << ", " << (S += D) << ", " << 0 << std::endl << std::flush;
       continue;
     }
-    const auto idS(one / dS);
-    const auto dd(idS - one / bdS);
+    const auto Sbd(Sd);
+    const auto iSd(one / (Sd += d));
+    const auto dd(iSd - one / Sbd);
     if(! isfinite(dd) || isnan(dd)) {
       std::cout << D << ", " << M << ", " << (S += D) << ", " << 0 << std::endl << std::flush;
       continue;
@@ -121,25 +123,46 @@ int main(int argc, const char* argv[]) {
     // N.B. compete with dimension the original function might have.
     //      (x, f(x), status, const.) is eliminated twice in this method
     //      for anti-symmetric ones.
-    const auto r(break_invariant0 ? one + num_t(int(t ++)) / num_t(int(abs(break_invariant0))) : one);
-    const auto pd(pp.next(break_invariant0 ? (break_invariant0 < 0 ? d  / r : d  * r) : d));
-    const auto qd(qq.next(break_invariant0 ? (break_invariant0 < 0 ? d  / r : d  * r) : d));
-    const auto pdd(p.next(break_invariant0 ? (break_invariant0 < 0 ? dd / r : dd * r) : dd));
-    const auto qdd(q.next(break_invariant0 ? (break_invariant0 < 0 ? dd / r : dd * r) : dd));
-    const auto pp2(pdd + idS);
-    const auto pq2(qdd + idS);
-    const auto pp3(pd  +  dS);
-    const auto pq3(qd  +  dS);
+    // N.B. compete with jammer, we average linear weighted and bared.
+    const auto r(one + num_t(int(t ++)) / num_t(int(abs(break_invariant0))));
+    const auto dr( break_invariant0 < 0 ? d  / r : d  * r);
+    const auto ddr(break_invariant0 < 0 ? dd / r : dd * r);
+    const auto iSr( one / (Sr  += dr));
+    const auto iSri(one / (Sri += ddr));
+    const auto pr(pp.next(dr));
+    const auto qr(qq.next(dr));
+    const auto pir(p.next(ddr));
+    const auto qir(q.next(ddr));
+    const auto p1(pp2.next(d));
+    const auto q1(qq2.next(d));
+    const auto pi1(p2.next(dd));
+    const auto qi1(q2.next(dd));
+    const auto pri(pir + iSr);
+    const auto qri(qir + iSr);
+    const auto p1i(pi1 + iSd);
+    const auto q1i(qi1 + iSd);
+    const auto piri(pr + Sr);
+    const auto qiri(qr + Sr);
+    const auto pi1i(p1 + Sd);
+    const auto qi1i(q1 + Sd);
     for(int i = 0; i < Mstore.cols() - 1; i ++)
       Mstore.setCol(i, Mstore.col(i + 1));
-    Mstore(0, Mstore.cols() - 1) =   pd;
-    Mstore(1, Mstore.cols() - 1) =   qd;
-    Mstore(2, Mstore.cols() - 1) = - pdd;
-    Mstore(3, Mstore.cols() - 1) = - qdd;
-    Mstore(4, Mstore.cols() - 1) = pp2 == zero ? pp2 :    one / std::move(pp2) - dS;
-    Mstore(5, Mstore.cols() - 1) = pq2 == zero ? pq2 :    one / std::move(pq2) - dS;
-    Mstore(6, Mstore.cols() - 1) = pp3 == zero ? pp3 : - (one / std::move(pp3) - idS);
-    Mstore(7, Mstore.cols() - 1) = pq3 == zero ? pq3 : - (one / std::move(pq3) - idS);
+    Mstore(0, Mstore.cols() - 1) =   pr;
+    Mstore(1, Mstore.cols() - 1) =   qr;
+    Mstore(2, Mstore.cols() - 1) =   p1;
+    Mstore(3, Mstore.cols() - 1) =   q1;
+    Mstore(4, Mstore.cols() - 1) = - pir;
+    Mstore(5, Mstore.cols() - 1) = - qir;
+    Mstore(6, Mstore.cols() - 1) = - p1i;
+    Mstore(7, Mstore.cols() - 1) = - q1i;
+    Mstore(8,  Mstore.cols() - 1) = pri  == zero ? pri  :    one / pri  - Sr;
+    Mstore(9,  Mstore.cols() - 1) = qri  == zero ? qri  :    one / qri  - Sr;
+    Mstore(10, Mstore.cols() - 1) = p1i  == zero ? p1i  :    one / p1i  - Sd;
+    Mstore(11, Mstore.cols() - 1) = q1i  == zero ? q1i  :    one / q1i  - Sd;
+    Mstore(12, Mstore.cols() - 1) = piri == zero ? piri : - (one / piri - iSr);
+    Mstore(13, Mstore.cols() - 1) = qiri == zero ? qiri : - (one / qiri - iSr);
+    Mstore(14, Mstore.cols() - 1) = pi1i == zero ? pi1i : - (one / pi1i - iSd);
+    Mstore(15, Mstore.cols() - 1) = qi1i == zero ? qi1i : - (one / qi1i - iSd);
     auto MMstore(Mstore);
     for(int i = 0; i < MMstore.rows(); i ++) {
       const auto norm2(MMstore.row(i).dot(MMstore.row(i)));
